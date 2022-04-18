52e89915cd99ef23f87e5f754b5ffe826311d0e2
Daniel Beeke <mail@danielbeeke.nl>
2022-04-18 11:33:06 +0200

    Adding patches that are incorrect but make the current use case work

--- src/CustomElements/FrmForm.ts
+++ src/CustomElements/FrmForm.ts
@@ -37,7 +37,7 @@ export const init = (settings: Settings) => {
       this.dataSubject = this.settings.context.expandTerm(this.dataSubject)!
 
       this.dataText = await resolveAttribute(this, 'data')
-      this.data = await rdfToLDflex(this.dataText, this.dataSubject)
+      this.data = await rdfToLDflex(this.dataText, this.dataSubject, true)
 
       this.classList.remove('loading')
 
--- src/helpers/rdfToLDflex.ts
+++ src/helpers/rdfToLDflex.ts
@@ -1,12 +1,30 @@
 import { rdfToStore } from './rdfToStore'
 import ComunicaEngine from '@ldflex/comunica'
-import { NamedNode } from 'n3'
+import { NamedNode, Writer } from 'n3'
 import basePrefixes from './basePrefixes'
 import { PathFactory, defaultHandlers } from 'ldflex'
 import defaultIterationHandlers from '@ldflex/async-iteration-handlers'
 
-export const rdfToLDflex = async (turtleShaclText, subjectUri: string) => {
+export const rdfToLDflex = async (turtleShaclText, subjectUri: string, dumpStore: boolean = false) => {
   const { store, prefixes } = await rdfToStore(turtleShaclText)
+  if (dumpStore) {
+    console.log(store)
+    console.log({
+      serialize: async () => {
+        const writer = new Writer({ prefixes: {...JSON.parse(JSON.stringify(prefixes)), ...basePrefixes}})
+        store.forEach(quad => writer.addQuad(quad), null, null, null, null)
+        writer.end((error, result) => console.log(result))
+      }
+    })
+  }
+
+  // const storeProxy = new Proxy(store, {
+  //   get: function (target, propertyKey, receiver) {
+  //     console.log(propertyKey)
+  //     return Reflect.get(target, propertyKey, receiver)
+  //   }
+  // })
+  
   const queryEngine = new ComunicaEngine([store])
   const context = { '@context': {...JSON.parse(JSON.stringify(prefixes)), ...basePrefixes} }
   const path = new PathFactory({ context, queryEngine, handlers: {
